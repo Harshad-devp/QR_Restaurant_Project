94a42ff60dd1c68bc6995fff29b860a1e68c2007
Harshad Raghwani <raghwaniharshad3@gmail.com>
2026-01-16 11:51:45 +0530

    Day 3: Dynamic menu page added

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+# Temprory database 
+menu_items = [
+    {"id": 1, "name": "Margherita Pizza", "price": 199},
+    {"id": 2, "name": "Veg Burger", "price": 99},
+    {"id": 3, "name": "Pasta", "price": 149},
+    {"id": 4, "name": "Cold Coffee", "price": 79}
+]
+# page rendring route or HOME route
+@app.route("/")
 def home():
-    return "QR Restaurant Ordering System - Day 1 Success 🚀"
+    return render_template("index.html")
 
-if __name__ == '__main__':
+# Route for menu html
+@app.route("/menu")
+def menu():
+    return render_template("menu.html", menu=menu_items)
+
+if __name__ == "__main__":
     app.run(debug=True)
