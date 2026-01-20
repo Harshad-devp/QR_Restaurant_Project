cbf25de2785df6eecd770ffb9400b8b879e51767
Harshad Raghwani <raghwaniharshad3@gmail.com>
2026-01-20 11:03:57 +0530

    Day 5: Session-based add to cart and cart page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
 # Temprory database 
 menu_items = [
@@ -18,6 +19,28 @@ def home():
 @app.route("/menu")
 def menu():
     return render_template("menu.html", menu=menu_items)
+
+@app.route("/add/<int:item_id>")
+def add_to_cart(item_id):
+    if "cart" not in session:
+        session["cart"] = []
+
+    session["cart"].append(item_id)
+    return redirect(url_for("menu"))
+
+@app.route("/cart")
+def cart():
+    cart_items = []
+    cart_ids = session.get("cart", [])
+
+    for id in cart_ids:
+        for item in menu_items:
+            if item["id"] == id:
+                cart_items.append(item)
+
+    return render_template("cart.html", cart=cart_items)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
