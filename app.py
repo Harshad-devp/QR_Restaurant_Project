616e744c1846d6482edd61955710dd5f3490b24b
Harshad Raghwani <raghwaniharshad3@gmail.com>
2026-01-21 10:52:02 +0530

    Day 7: Cart page, grand total, and clean menu-cart separation

--- app.py
+++ app.py
@@ -3,33 +3,33 @@ from flask import Flask, render_template, session, redirect, url_for
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
-# Temprory database 
+# Temporary database
 menu_items = [
     {"id": 1, "name": "Margherita Pizza", "price": 199},
     {"id": 2, "name": "Veg Burger", "price": 99},
     {"id": 3, "name": "Pasta", "price": 149},
     {"id": 4, "name": "Cold Coffee", "price": 79}
 ]
-# page rendring route or HOME route
+
+# Home route
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Route for menu html
+# Menu route
 @app.route("/menu")
 def menu():
     return render_template("menu.html", menu=menu_items)
 
+# Add to cart
 @app.route("/add/<int:item_id>")
 def add_to_cart(item_id):
 
-    # FORCE reset if cart is list (Day 5 leftover)
     if "cart" not in session or isinstance(session["cart"], list):
         session["cart"] = {}
 
     cart = session["cart"]
 
-    # Get correct price from menu_items
     price = 0
     for item in menu_items:
         if item["id"] == item_id:
@@ -47,6 +47,43 @@ def add_to_cart(item_id):
     session["cart"] = cart
     return redirect(url_for("menu"))
 
+# Remove from cart
+@app.route("/remove/<int:item_id>")
+def remove_from_cart(item_id):
+
+    if "cart" in session:
+        cart = session["cart"]
+        cart.pop(str(item_id), None)
+        session["cart"] = cart
+
+    return redirect(url_for("menu"))
+
+# Cart page
+@app.route("/cart")
+def cart():
+
+    cart_data = []
+    grand_total = 0
+
+    if "cart" in session:
+        for item_id, data in session["cart"].items():
+            for item in menu_items:
+                if item["id"] == int(item_id):
+                    item_total = data["price"] * data["qty"]
+                    grand_total += item_total
+                    cart_data.append({
+                        "id": item["id"],
+                        "name": item["name"],
+                        "price": data["price"],
+                        "qty": data["qty"],
+                        "total": item_total
+                    })
+
+    return render_template(
+        "cart.html",
+        cart=cart_data,
+        grand_total=grand_total
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
